843fd5173f1e6c1aec42cc72e593c752ccdc81a2
workspace-tj <workspace.tj@gmail.com>
2024-07-20 22:04:04 +0900

    :sparkles: define customer model

---
+++ src/models/customer.py
@@ -0,0 +1,17 @@
+from pynamodb.models import Model
+from pynamodb.attributes import (
+    UnicodeAttribute, NumberAttribute, UnicodeSetAttribute, UTCDateTimeAttribute
+)
+
+from utils.generate_id import generate_id
+from datetime import datetime
+
+class Customer(Model):
+    class Meta:
+        table_name = 'Customer'
+
+    id = UnicodeAttribute(hash_key=True, default=generate_id)
+    email = UnicodeAttribute(range_key=True, null=False)
+    username = UnicodeAttribute(range_key=True)
+    created_at = UTCDateTimeAttribute(default=datetime.now)
+    updated_at = UTCDateTimeAttribute(default=datetime.now)
