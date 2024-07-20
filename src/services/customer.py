f0574d9176a3b793c480fa83a9b94b5039452fad
workspace-tj <workspace.tj@gmail.com>
2024-07-20 23:06:35 +0900

    :sparkles: add cusomter service

---
+++ src/services/customer.py
@@ -0,0 +1,15 @@
+from typing import List
+
+from models.customer import CustomerModel
+from repositories.customer import CustomerRepository
+from schemas.customer import CustomerSchema
+
+
+class CustomerService:
+    def __init__(self) -> None:
+        # CustomerRepositoryをインスタンス化
+        self.customer_repository = CustomerRepository()
+        
+    def find_all(self) -> List[CustomerSchema]:
+        items = self.customer_repository.find_all()
+        return [CustomerModel(**item) for item in items]
