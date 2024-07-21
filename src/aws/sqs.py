1b98c1d8bc08581024c6e6c4bfead215c4bd8e82
workspace-tj <workspace.tj@gmail.com>
2024-07-21 14:10:14 +0900

    :shirt:fix  env import

--- src/aws/sqs.py
+++ src/aws/sqs.py
@@ -2,11 +2,10 @@ from typing import Any
 
 import boto3
 
-from .env import QUEUE_URL
-
+from src.env.main import SQS_QUEUE_URL
 
 class SQSClient:
-    def __init__(self, queue_url: str = QUEUE_URL) -> None:
+    def __init__(self, queue_url: str = SQS_QUEUE_URL) -> None:
         self.queue_url = queue_url
         self.client = boto3.client("sqs")
         
