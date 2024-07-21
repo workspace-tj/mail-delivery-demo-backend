7f574b3b88b75fdf6a6dc36cbc1a561641fb5034
workspace-tj <workspace.tj@gmail.com>
2024-07-21 21:24:32 +0900

    :shirt:: enable to add local queue

--- src/aws/sqs.py
+++ src/aws/sqs.py
@@ -4,13 +4,25 @@ import json
 import boto3
 
 from src.env.main import SQS_QUEUE_URL
+from config.api import STAGE
 
 
 class SQSClient:
     def __init__(self, queue_url: str = SQS_QUEUE_URL) -> None:
         self.queue_url = queue_url
+        if STAGE == "local":
+            session = boto3.session.Session()
+            self.client=session.client(
+                service_name='sqs',
+                aws_access_key_id='aaa',
+                aws_secret_access_key='bbb',
+                endpoint_url=SQS_QUEUE_URL,
+                )
+            return
         self.client = boto3.client("sqs")
 
+
     def send_message(self, message_body: Dict[str, Any]) -> Dict[str, Any]:
-        response = self.client.send_message(QueueUrl=self.queue_url, MessageBody=json.dumps(message_body))
+        # FIXME: queurl
+        response = self.client.send_message(QueueUrl=self.queue_url + "/test-que", MessageBody=json.dumps(message_body))
         return response
